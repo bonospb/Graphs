5cf39eba5075fbdef4f32c0e44ca55c5ec2b01a3
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2023-05-30 04:21:15 +0300

    исправления по алгоритму Буллмана-Форда

--- src/Graph/Search/Bellman-Ford/BellmanFordAlgorithm.cs
+++ src/Graph/Search/Bellman-Ford/BellmanFordAlgorithm.cs
@@ -16,31 +16,31 @@ namespace FreeTeam.Graph.Search
             _graph = graph;
 
         #region Public methods
-        public void BellmanFord(T start)
+        public void BellmanFord(T start, T finish)
         {
-            var edges = _graph.Vertices.SelectMany(x => x.Edges.Select(y => new GraphEdgeInfo<T>(x, y.ConnectedNode, y.Weight)));
+            var edges = _graph.Vertices
+                .SelectMany(x => x.Edges.Select(y => new GraphEdgeInfo<T>(x, y.ConnectedNode, y.Weight)))
+                .Distinct()
+                .ToArray();
 
             int verticesCount = _graph.Count;
             int edgesCount = edges.Count();
 
             Dictionary<T, int> distance = new(verticesCount);
+            Dictionary<T, T> parent = new(verticesCount);
 
             foreach (var vertex in _graph.Vertices)
+            {
                 distance.Add(vertex.Value, int.MaxValue);
+                parent.Add(vertex.Value, default);
+            }
 
             distance[start] = 0;
 
             for (int i = 1; i < verticesCount; ++i)
             {
                 foreach (var edge in edges)
-                {
-                    T u = edge.Source.Value;
-                    T v = edge.Destination.Value;
-                    int weight = edge.Weight;
-
-                    if (distance[u] != int.MaxValue && distance[u] + weight < distance[v])
-                        distance[v] = distance[u] + weight;
-                }
+                    Relax(edge.Source.Value, edge.Destination.Value, edge.Weight, distance, parent);
             }
 
             foreach (var edge in edges)
@@ -50,22 +50,32 @@ namespace FreeTeam.Graph.Search
                 int weight = edge.Weight;
 
                 if (distance[u] != int.MaxValue && distance[u] + weight < distance[v])
-                    Debug.Log("Graph contains negative weight cycle.");
+                    Debug.Log($"Graph contains negative weight cycle! Edge: {edge.Source.Value} <-> {edge.Destination.Value}, Weight: ({edge.Weight})");
             }
 
-            var str = "Results\n";
-            foreach (var d in distance)
-                str += $"Vertex: {d.Key}, Distance: {d.Value}\n";
-
-            Debug.Log(str);
+            PrintPath(start, finish, distance, parent);
         }
         #endregion
 
         #region Private methods
-        private IEnumerable<T> GetPath()
+        private void Relax(T u, T v, int weight, Dictionary<T, int> distance, Dictionary<T, T> parent)
         {
-            var path = new LinkedList<T>();
-            return path;
+            if (distance[u] != int.MaxValue && distance[v] > distance[u] + weight)
+            {
+                distance[v] = distance[u] + weight;
+                parent[v] = u;
+            }
+        }
+
+        private void PrintPath(T u, T v, Dictionary<T, int> distance, Dictionary<T, T> parent)
+        {
+            if (v.Equals(default) || u.Equals(default))
+                return;
+
+            if (!v.Equals(u))
+                PrintPath(u, parent[v], distance, parent);
+
+            Debug.Log($"Vertex {v} weight: {distance[v]}");
         }
         #endregion
     }
--- src/Graph/Search/Bellman-Ford/GraphEdgeInfo.cs
+++ src/Graph/Search/Bellman-Ford/GraphEdgeInfo.cs
@@ -1,6 +1,9 @@
-﻿namespace FreeTeam.Graph.Search
+﻿using Mono.Cecil.Cil;
+using System;
+
+namespace FreeTeam.Graph.Search
 {
-    public class GraphEdgeInfo<T>
+    public class GraphEdgeInfo<T> : IEquatable<GraphEdgeInfo<T>>
     {
         #region Public
         public Vertex<T> Source { get; }
@@ -13,6 +16,33 @@
             Source = source;
             Destination = destination;
             Weight = weight;
+        }
+
+        public bool Equals(GraphEdgeInfo<T> obj)
+        {
+            if (obj is null) 
+                return false;
+
+            if (Object.ReferenceEquals(this, obj)) 
+                return true;
+
+            return ((Source.Value.Equals(obj.Destination.Value)
+            &&
+            Destination.Value.Equals(obj.Source.Value))
+            ||
+            (Source.Value.Equals(obj.Source.Value)
+            &&
+            Destination.Value.Equals(obj.Destination.Value)))
+            && Weight == obj.Weight;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashSourceValue = Source.Value == null ? 0 : Source.Value.GetHashCode();
+            int hashDestinationValue = Destination.Value == null ? 0 : Destination.Value.GetHashCode();
+            int hashProductCode = Weight.GetHashCode();
+
+            return hashSourceValue ^ hashDestinationValue ^ hashProductCode;
         }
     }
 }
